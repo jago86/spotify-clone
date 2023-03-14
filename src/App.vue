336ab205e2d493fb8f2fa0537a13842fccb5967f
Jairo Ushiña <jago86@gmail.com>
2023-03-13 22:16:39 -0500

    M1. E3.

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,7 @@
 <template>
-  Hola Tailwind
+  <div class="relative h-screen flex text-white">
+    <div class="w-[13%] px-3 py-2 bg-black">Sidebar</div>
+    <div class="w-[87%] px-3 py-2 bg-neutral-900">Content</div>
+    <div class="absolute w-full bg-neutral-900 bottom-0 h-16 border-t border-neutral-700">Playback bar</div>
+  </div>
 </template>
