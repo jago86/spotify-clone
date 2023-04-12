469f2a7613c01c6c909e3d03490cb855bdfb1917
Jairo Ushiña <jago86@gmail.com>
2023-04-12 10:20:47 -0500

    M2. E3.

--- src/App.vue
+++ src/App.vue
@@ -102,63 +102,24 @@
 
         <div class="mt-8 grid grid-rows-1 grid-cols-5 gap-x-5">
 
-          <div class="group h-[17rem] bg-dark hover:bg-dark-light rounded cursor-pointer">
-            <div class="relative">
-              <div class="w-36 h-36 mx-auto mt-4 rounded-full overflow-hidden">
-                <!-- <img class="mx-auto mt-4 rounded-full" src="/images/sacoto.jpeg" alt=""> -->
-                <img class="w-full h-full object-cover" src="/images/sacoto.jpeg" alt="">
-              </div>
-              <button class="absolute right-5 bottom-1 opacity-0 group-hover:opacity-100 translate-y-5 group-hover:translate-y-0 transition">
-                <svg class="w-16 h-16 text-green-primary" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M15.91 11.672a.375.375 0 010 .656l-5.603 3.113a.375.375 0 01-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112z" />
-                </svg>
-              </button>
-            </div>
-            <div class="mx-5 mt-4">
-              <h3 class="font-bold text-light-primary">Sergio Sacoto</h3>
-              <p class="text-sm text-light-secondary">Artista</p>
-            </div>
-          </div>
+          <Tile
+            title="Sergio Sacoto"
+            sub-title="Artista"
+            image="/images/sacoto.jpeg"
+          ></Tile>
 
-          <div class="group h-[17rem] bg-dark hover:bg-dark-light rounded cursor-pointer">
-              <div class="relative">
-                <div class="w-36 h-36 mx-auto mt-4 rounded-full overflow-hidden">
-                  <!-- <img class="mx-auto mt-4 rounded-full" src="/images/sacoto.jpeg" alt=""> -->
-                  <img class="w-full h-full object-cover" src="/images/enanitos.jpeg" alt="">
-                </div>
-                <button class="absolute right-5 bottom-1 opacity-0 group-hover:opacity-100 translate-y-5 group-hover:translate-y-0 transition">
-                  <svg class="w-16 h-16 text-green-primary" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.91 11.672a.375.375 0 010 .656l-5.603 3.113a.375.375 0 01-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112z" />
-                  </svg>
-                </button>
-              </div>
-              <div class="mx-5 mt-4">
-                <h3 class="font-bold text-light-primary">Enanitos Verdes</h3>
-                <p class="text-sm text-light-secondary">Artista</p>
-              </div>
-            </div>
+          <Tile
+            title="Enanitos Verdes"
+            sub-title="Artista"
+            image="/images/enanitos.jpeg"
+          ></Tile>
 
-
-            <div class="group h-[17rem] bg-dark hover:bg-dark-light rounded cursor-pointer">
-                <div class="relative">
-                  <div class="w-36 h-36 mx-auto mt-4 rounded overflow-hidden">
-                    <!-- <img class="mx-auto mt-4 rounded-full" src="/images/sacoto.jpeg" alt=""> -->
-                    <img class="w-full h-full object-cover" src="/images/rock.jpeg" alt="">
-                  </div>
-                  <button class="absolute right-5 bottom-1 opacity-0 group-hover:opacity-100 translate-y-5 group-hover:translate-y-0 transition">
-                    <svg class="w-16 h-16 text-green-primary" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                      <path stroke-linecap="round" stroke-linejoin="round" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                      <path stroke-linecap="round" stroke-linejoin="round" d="M15.91 11.672a.375.375 0 010 .656l-5.603 3.113a.375.375 0 01-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112z" />
-                    </svg>
-                  </button>
-                </div>
-                <div class="mx-5 mt-4">
-                  <h3 class="font-bold text-light-primary">00s Rock Anthems</h3>
-                  <p class="text-sm text-light-secondary">These songs rocked the 00s. Cover: Green Day</p>
-                </div>
-              </div>
+          <Tile
+            title="00s RockAnthems"
+            sub-title="These songs rocked the 00s. Cover: Green Day"
+            image="/images/rock.jpeg"
+            shape="square"
+          ></Tile>
 
         </div>
       </div>
@@ -275,3 +236,7 @@
 
   </div>
 </template>
+
+<script setup>
+import Tile from './components/Tile.vue';
+</script>
