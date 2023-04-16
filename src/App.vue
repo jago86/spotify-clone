1e16143f3614bd4a20fbc69f8763ba5b34a30fbf
Jairo Ushiña <jago86@gmail.com>
2023-04-16 11:52:37 -0500

    M2. E8.

--- src/App.vue
+++ src/App.vue
@@ -144,9 +144,9 @@
             <button class="text-xs hover:underline block">Sergio Sacoto</button>
           </div>
           <div>
-            <button>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" /></svg>
-            </button>
+            <Like
+              :like="true"
+            ></Like>
           </div>
 
         </div>
@@ -241,5 +241,6 @@
 
 <script setup>
 import Tile from './components/Tile.vue';
+import Like from './components/Like.vue';
 import Dropdown from './components/Dropdown.vue';
 </script>
